62a98836a9b0019dd026c1191f1ae12983f8c9c5
Carlos Fonseca <cfonseca@blissapplications.com>
2025-02-25 09:11:22 +0000

    Shows the name of the current top view controller on long press of the floating button

--- DebugSwift/Sources/Helpers/Managers/WindowManager.swift
+++ DebugSwift/Sources/Helpers/Managers/WindowManager.swift
@@ -68,7 +68,7 @@ enum WindowManager {
 
         let alertController = UIAlertController(
             title: "Select a Window",
-            message: nil,
+            message: topViewController().map { String(describing: type(of: $0)) },
             preferredStyle: .actionSheet
         )
 
@@ -133,5 +133,23 @@ final class CustomWindow: UIWindow {
         }
 
         return false
+    }
+}
+
+extension WindowManager {
+    static func topViewController(controller: UIViewController? = UIApplication.shared.windows.first?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
     }
 }
